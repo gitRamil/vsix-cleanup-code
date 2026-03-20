8d90cd4a632ff39732830884c26b5f542a40e2ee
Ramil <rali@cryptopro.ru>
2026-03-20 12:39:41 +0300

    Add command.

--- JBCleanupExtension/JBCleanupExtensionPackage.cs
+++ JBCleanupExtension/JBCleanupExtensionPackage.cs
@@ -25,6 +25,7 @@ namespace JBCleanupExtension
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(JBCleanupExtensionPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class JBCleanupExtensionPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace JBCleanupExtension
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await CleanupCommand.InitializeAsync(this);
         }
 
         #endregion
